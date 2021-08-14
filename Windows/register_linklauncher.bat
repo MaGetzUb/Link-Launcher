a50b2f41e06e2ebee14b6264b626671bcd9f8fce
Marko <marko_ranta@protonmail.com>
2021-08-14 19:36:35 +0300

    Added register_linklauncher.bat

---
+++ Windows/register_linklauncher.bat
@@ -0,0 +1,18 @@
+@echo off
+
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities" /v "ApplicationDescription" /t REG_SZ /d "Launch Links to different browsers" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities" /v "ApplicationName" /t REG_SZ /d "Lin kLauncher" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities" /v "ApplicationIcon" /t REG_SZ /d "%LINK_LAUNCHER%\LinkLauncher.exe,0" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\FileAssociations" /v ".htm" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\FileAssociations" /v ".html" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\FileAssociations" /v ".shtml" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\FileAssociations" /v ".xht" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\FileAssociations" /v ".xhtml" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\URLAssociations" /v "ftp" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\URLAssociations" /v "http" /t REG_SZ /d "LinkLauncherURL" /f
+Reg.exe add "HKLM\SOFTWARE\Link Launcher\Capabilities\URLAssociations" /v "https" /t REG_SZ /d "LinkLauncherURL" /f
+REM ;Register as default program
+Reg.exe add "HKLM\SOFTWARE\RegisteredApplications" /v "Link Launcher" /t REG_SZ /d "Software\Link Launcher\Capabilities" /f
+Reg.exe add "HKLM\Software\Classes\LinkLauncherURL" /ve /t REG_SZ /d "Web Browser Document" /f
+Reg.exe add "HKLM\Software\Classes\LinkLauncherURL" /v "FriendlyTypeName" /t REG_SZ /d "Web Browser Document" /f
+Reg.exe add "HKLM\Software\Classes\LinkLauncherURL\shell\open\command" /ve /t REG_SZ /d "\"%LINK_LAUNCHER%\LinkLauncher.exe\" \"%%1\"" /f
